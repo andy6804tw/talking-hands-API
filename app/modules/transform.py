485dfad0160b285059611f8d9f35b317561bfb37
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-11-30 11:53:04 +0800

    use gpu build model

--- app/modules/transform.py
+++ app/modules/transform.py
@@ -10,15 +10,17 @@ from face_alignment import FaceAlignment, LandmarksType
 from app.modules.talkingHeads.utils import load_model, generate_image, plot_landmarks, generate_lm, image_to_video, generate_moving_video
 import app.modules.talkingHeads.network as network
 
+from flask import jsonify
+
 # %load_ext autoreload
 # %autoreload 2
 
 def getTransform():
-  G = network.Generator()
-  G = load_model(G, "app/modules/talkingHeads/resource/han", "han")
-  G = G.to("cpu")
-  generate_moving_video(G, "app/modules/talkingHeads/resource/demo.mov", "app/modules/talkingHeads/resource/han/han.npy", "app/static/test.mp4", "cpu")
-  return 'ddon'
+    G = network.Generator()
+    G = load_model(G, "app/modules/talkingHeads/resource/han", "han")
+    G = G.to("cuda:0")
+    generate_moving_video(G, "app/static/source.mp4", "app/modules/talkingHeads/resource/han/han.npy", "app/static/result.mp4", "cuda:0")
+    return jsonify({"code":200,"message": "轉換成功"})
 
 def getResult():
     return 'ddon'
