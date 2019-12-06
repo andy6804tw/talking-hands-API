3b391687ff065ce30eb718bd08c3f5b99a1cff07
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-06 21:56:57 +0800

    modify transform api

--- app/modules/transform.py
+++ app/modules/transform.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import torchvision.transforms.functional as TF
 from face_alignment import FaceAlignment, LandmarksType
 
-from app.modules.talkingHeads.utils import load_model, generate_image, plot_landmarks, generate_lm, image_to_video, generate_moving_video
+from app.modules.talkingHeads.utils import load_model, generate_image, plot_landmarks, generate_lm, image_to_video, generate_moving_video, generate_moving_image, get_e_vector
 import app.modules.talkingHeads.network as network
 
 from flask import jsonify
@@ -21,9 +21,12 @@ def getTransform(videoName,modelIdx):
     G = network.Generator()
     G = load_model(G, "app/modules/talkingHeads/resource/"+modelList[modelIdx], modelList[modelIdx])
     G = G.to("cuda:0")
+    fa = FaceAlignment(LandmarksType._2D, device='cuda:0')
+    e_vector = get_e_vector("app/modules/talkingHeads/resource/"+modelList[modelIdx]+"/"+modelList[modelIdx]+".npy")
     timestamp=str(int(time.time()))
     print(timestamp)
-    generate_moving_video(G, "app/static/"+videoName, "app/modules/talkingHeads/resource/"+modelList[modelIdx]+"/"+modelList[modelIdx]+".npy", "app/static/result-"+timestamp+".mp4", "cuda:0")
+#     generate_moving_video(G, "app/static/"+videoName, "app/modules/talkingHeads/resource/"+modelList[modelIdx]+"/"+modelList[modelIdx]+".npy", "app/static/result-"+timestamp+".mp4", "cuda:0")
+    generate_moving_video(G, "app/static/"+videoName, e_vector, "app/static/result-"+timestamp+".mp4", "cuda:0", fa)
     return jsonify({"code":200,"message": "轉換成功",'token':timestamp})
 
 def getResult():
